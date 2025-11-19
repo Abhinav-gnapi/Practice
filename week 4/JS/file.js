9c7e32d9d8dc1e6b9ca646a3bd8f1a1cc0388bf1
Abhinav <abhinav.k@gnapi.tech>
2025-11-19 13:01:24 +0530

    initial commit

---
+++ week 4/JS/file.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+console.log(typeof("hello"));
+console.log(typeof("123"));
+console.log(typeof(112));
+console.log(typeof(3.2));
+console.log(typeof(0/2));
+console.log(typeof(2/0));
+console.log(typeof(true));
+
