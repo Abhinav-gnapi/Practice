92b25fed5029b67747f85c0d864f52a8e1ce3c0d
Abhinav <abhinav.k@gnapi.tech>
2025-12-11 13:59:04 +0530

    material-ui added to the project and icons added to the page

--- React/To-do/src/components/ListButton.jsx
+++ React/To-do/src/components/ListButton.jsx
@@ -1,7 +1,9 @@
+import DeleteIcon from '@mui/icons-material/Delete';
+
 function ListButton(props){
     return(
         <button onClick={() => {
-            props.onChecked(props.id)}}>Delete</button>
+            props.onChecked(props.id)}}><DeleteIcon className='deleteButtons'/></button>
     )
 }
 export default ListButton
